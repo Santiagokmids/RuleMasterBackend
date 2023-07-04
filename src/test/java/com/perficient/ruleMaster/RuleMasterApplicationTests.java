2b858658be1d4c46359fb42024b4a9341dd79a60
AngelicaCorrales <72984945+AngelicaCorrales@users.noreply.github.com>
2023-07-04 17:33:23 -0500

    test: RuleApplicationTests passed

--- src/test/java/com/perficient/ruleMaster/RuleMasterApplicationTests.java
+++ src/test/java/com/perficient/ruleMaster/RuleMasterApplicationTests.java
@@ -2,8 +2,12 @@ package com.perficient.ruleMaster;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(profiles="test")
+@Import(TestConfigurationData.class)
 class RuleMasterApplicationTests {
 
 	@Test
