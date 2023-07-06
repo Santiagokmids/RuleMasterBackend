f9f4d163047f92a7840a82f074f9da977d27d583
Santiagokmids <sntgtrochez@gmail.com>
2023-07-05 23:06:24 -0500

    feat: api documentation added

--- src/main/java/com/perficient/ruleMaster/RuleMasterApplication.java
+++ src/main/java/com/perficient/ruleMaster/RuleMasterApplication.java
@@ -9,10 +9,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.UUID;
 
 @SpringBootApplication
+@EnableSwagger2
 public class RuleMasterApplication {
 
 	public static void main(String[] args) {
